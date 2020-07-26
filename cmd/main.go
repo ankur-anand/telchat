83024a3fe5ae11d8195678dd07fee4dffefc6746
ankur-anand <ankur.anand12@gmail.com>
2020-07-26 19:57:35 +0530

    read config from arguments

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/signal"
@@ -8,20 +11,40 @@ import (
 	"github.com/ankur-anand/telchat/pkg"
 )
 
+var configLocation = flag.String("config", "./config.json", "config.json file location")
+
+type config struct {
+	LogFile    string `json:"log_file"`
+	TelnetAddr string `json:"telnet_addr"`
+	HTTPAddr   string `json:"http_addr"`
+}
+
 func main() {
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
 	log.SetFlags(log.Lshortfile | log.LstdFlags | log.Lmicroseconds | log.LUTC | log.Lmsgprefix)
 	log.SetPrefix("[telchat] ")
-	dir := os.TempDir()
-	file := dir + "/telchat.log"
-	cs, err := pkg.NewChatServer(file)
-	log.Printf("log will be written to %s \n", file)
+
+	flag.Parse()
+	// read file.
+	cb, err := ioutil.ReadFile(*configLocation)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var cg config
+	err = json.Unmarshal(cb, &cg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	cs, err := pkg.NewChatServer(cg.LogFile)
+	log.Printf("log will be written to %s \n", cg.LogFile)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	go cs.ServeHTTP(":3002")
-	go cs.ServeTelnet(":3001")
+	go cs.ServeHTTP(cg.HTTPAddr)
+	go cs.ServeTelnet(cg.TelnetAddr)
 
 	<-c
 	log.Printf("shutting down server")
